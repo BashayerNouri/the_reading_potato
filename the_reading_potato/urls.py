b04e692b025029bd43c0d556442d8b1e474b2585
Bashayer Nouri <bashayer_nouri@hotmail.com>
2020-02-11 15:00:02 +0400

    basic edits

--- the_reading_potato/urls.py
+++ the_reading_potato/urls.py
@@ -13,6 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from main import views
@@ -22,3 +25,6 @@ urlpatterns = [
     path('articles/', views.articles_list, name="articles-list"),
     path('articles/<int:article_id>/', views.article_details, name="article-details"),
 ]
+
+
+urlpatterns+=static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
