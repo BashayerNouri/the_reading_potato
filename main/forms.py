312c767d6a9d183f91888ce089f84d3bf43045d5
Bashayer Nouri <bashayer_nouri@hotmail.com>
2020-02-13 14:26:09 +0400

    finished contributing functionality

--- main/forms.py
+++ main/forms.py
@@ -1,7 +1,14 @@
 from django import forms
 from .models import Article
 
+
 class ArticleForm(forms.ModelForm):
 	class Meta:
 		model = Article
 		fields = ['title', 'content']
+
+
+class ContributeArticleForm(forms.ModelForm):
+    class Meta:
+        model = Article
+        fields = ['content']
