697747f06760d86b9f506e34ba2f00768686a43d
Bashayer Nouri <bashayer_nouri@hotmail.com>
2020-02-16 12:38:37 +0400

    added slugs

--- main/views.py
+++ main/views.py
@@ -28,8 +28,8 @@ def articles_list(request):
     return render(request, "articles_list.html", context)
 
 
-def article_details(request, article_id):
-    article = Article.objects.get(id=article_id)
+def article_details(request, article_slug):
+    article = Article.objects.get(slug=article_slug)
     # distinct() does not work with local databse, this will work with the deployement database.
     if settings.DEBUG:
         contributions = article.contributions.filter(status=Contribution.ACCEPTED)
@@ -54,28 +54,28 @@ def create_article(request):
             article = form.save(commit=False)
             article.author = request.user
             article.save()
-            return redirect('article-details', article.id)
+            return redirect('article-details', article.slug)
 
     context = {"form" : form}
 
     return render(request, "create_article.html", context)
 
 
-def edit_article(request, article_id):
+def edit_article(request, article_slug):
     if request.user.is_anonymous:
         return redirect('login')
 
-    article = Article.objects.get(id=article_id)
+    article = Article.objects.get(slug=article_slug)
 
     if article.author != request.user:
-        return redirect('article-details', article_id)
+        return redirect('article-details', article_slug)
 
     form = ArticleForm(instance=article)
     if request.method == "POST":
         form = ArticleForm(request.POST, instance=article)
         if form.is_valid():
             form.save()
-            return redirect('article-details', article_id)
+            return redirect('article-details', article_slug)
 
     context = {"form":form, "article":article}
     return render(request, 'edit_article.html', context)
@@ -88,14 +88,14 @@ def my_articles_list(request):
     return render(request, "my_articles_list.html")
 
 
-def contribute_to_article(request, article_id):
+def contribute_to_article(request, article_slug):
     if request.user.is_anonymous:
         return redirect('login')
         
-    article = Article.objects.get(id=article_id)
+    article = Article.objects.get(slug=article_slug)
     
     if article.author == request.user:
-        return redirect('edit-article', article_id)
+        return redirect('edit-article', article_slug)
 
     form = ContributeArticleForm(instance=article)
     if request.method == "POST":
